1aa4cc9b6db9478c3fff71250ddb2a028cd258c9
Ernesto Diaz <diaz37@usf.edu>
2025-09-15 00:01:26 -0400

    second commit

---
+++ cc_3a.js
@@ -0,0 +1,51 @@
+// Define an array of customers
+let customers = [
+    {
+        custName: "John",
+        email: "john@example.com",
+        purchases:[
+            "laptop",
+            "smartphone",
+            "headphones"
+        ]
+    },
+    {
+        custName: "Jane",
+        email: "jane@example.com",
+        purchases:[
+            "tablet",
+            "smartwatch"
+        ]
+    },
+    {
+        custName: "Kevin",
+        email: "kevin@example.com",
+        purchases:[
+            "speaker",
+            "charger"
+        ]
+    }
+]
+
+// Add customer to the array
+customers.push({
+    custName: "Alice",
+    email: "alice@example.com",
+    purchases:[
+        "tablet",
+        "smartwatch"
+    ]
+});
+
+// Remove first customer from the array
+customers.shift();
+
+// Update customer info
+customers[1].email = "janey123@example.com"
+
+// Add a new purchase to a customer
+customers[2].purchases.push("smartwatch band");
+
+// Display customer information
+customers.forEach(customer => console.log(`${customer.custName}\t|\t${customer.email}\t|\t${customer.purchases.length}`));
+
